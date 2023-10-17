47875a4925e55efdc1d17c122069f09d743a23a4
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2023-10-16 22:22:07 -0300

    Versão final

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ function carregar (){
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora > 0 && hora <12){
+        document.body.style.background = '#cefc4f'
         img.src = 'manha.jpg'
     } else if (hora > 12 && hora <18){
+        document.body.style.background = '#4ffcdc'
         img.src = 'tarde.jpg'
     } else {
         img.src = 'noite.jpg'
+        document.body.style.background =  '#215b50'
     }
 
 }
-
